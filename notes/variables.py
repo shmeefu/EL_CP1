13f26f8343f9e53434706518668b193c1697c486
shmeefu <evie.lewis@ucas-edu.net>
2025-08-21 10:50:59 -0600

    variables

---
+++ notes/variables.py
@@ -0,0 +1,33 @@
+# EL 2nd Variables Notes
+
+# Syntax tells you how to set up your code. Syntax = grammar
+# name=content
+# The name is how we access the variable, no spaces are allowed, specific
+# Variable names have to begin with a letter or underscore
+# Variable names cannot contain special characters
+# All variable names must be unique within the program
+# The content is the value that the program uses
+# Declaration - Making the variable exsist
+# Initialization - put initial value in the variable aka the first thing you put in it
+
+
+student_count=29
+amount_of_water= "1 oz"
+name="bichael"
+grade=98
+grade=grade-10
+grade=88
+
+
+# What is a variable? A container that holds info.
+# List the steps for writing a variable: Name it, put the assignment operator (=) after, then put the content after
+# What is the difference between declaration and initialization of a variable? Declaration is making the variable exist, initialization is the
+# first thing you put in the variable
+# How do you reset a variable? Just rewrite it?
+# How do you print variables? print(variable name)
+# Why are variables important in programming?
+# What are the 3 naming systems used in programming?
+# Why can't we have spaces when we name things in python?
+# What things should you think about when coming up with a variable or function name?
+# Why must variable names be specific?
+# Why should names be short?
