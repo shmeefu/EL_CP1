3cb7b86a0dedb3e77092c5241920daa2e67073e8
Evie Lewis <evie.lewis@ucas-edu.net>
2025-09-05 21:43:04 -0600

    who are you

---
+++ starters/who_are_you.py
@@ -0,0 +1,8 @@
+# EL 2nd Who Are You
+
+name=input("What is your name?: ").capitalize()
+age=input ("How old are you?: ")
+color=input("What is your favorite color?: ").lower()
+
+sentence= "Hello, " + name + ", aged " + age + ". I like the color " + color + " too."
+print(sentence)
